c5de2136584af79b9b5d631c5596f4913f350b56
Hadi <hadibakhshi277@gmail.com>
2022-01-19 13:29:23 +0330

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter,Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Hello React</h2>
-    </div>
-  );
+  return <BrowserRouter>
+  <Route path="/" exact={true} component={HomePage}/>
+  <Route path="/about-us" component={AboutUsPage}/>
+  </BrowserRouter>;
 }
 
 export default App;
+ 
